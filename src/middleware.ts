67650e422b2bf74b7d17d9400e342cdf9e3074eb
Noch22 <noah.fakeadresse@gmail.com>
2025-05-21 22:55:54 +0200

    feat(auth): adding middleware forcing connection

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { betterFetch } from "@better-fetch/fetch";
+import { authClient } from "../lib/auth-client";
+import { NextRequest, NextResponse } from "next/server";
+ 
+type Session = typeof authClient.$Infer.Session;
+ 
+export async function middleware(request: NextRequest) {
+	const { data: session } = await betterFetch<Session>("/api/auth/get-session", {
+		baseURL: request.nextUrl.origin,
+		headers: {
+			cookie: request.headers.get("cookie") || "", // Forward the cookies from the request
+		},
+	});
+ 
+	if (!session) {
+		return NextResponse.redirect(new URL("/signin", request.url));
+	}
+ 
+	return NextResponse.next();
+}
+ 
+export const config = {
+	matcher: ["/"], // Apply middleware to specific routes
+};
