d6becb88e6eede52564a45cb865536b24c2d9529
Quentin Macheda <contact@qmweb.fr>
2025-05-21 18:21:44 +0200

    chore(global): add inter font

--- src/client/fonts/index.ts
+++ src/client/fonts/index.ts
@@ -1,79 +1,99 @@
 import localFont from 'next/font/local';
 
-export const Gotham = localFont({
+export const Inter = localFont({
   src: [
     {
-      path: './gotham/Gotham-Black.woff2',
-      weight: '900',
-      style: 'normal',
-    },
-    {
-      path: './gotham/Gotham-BlackItalic.woff2',
-      weight: '900',
-      style: 'italic',
-    },
-    {
-      path: './gotham/Gotham-Bold.woff2',
-      weight: '700',
-      style: 'normal',
-    },
-    {
-      path: './gotham/Gotham-BoldItalic.woff2',
-      weight: '700',
-      style: 'italic',
-    },
-    {
-      path: './gotham/Gotham-Book.woff2',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: './gotham/Gotham-BookItalic.woff2',
-      weight: '400',
-      style: 'italic',
-    },
-    {
-      path: './gotham/Gotham-Light.woff2',
-      weight: '300',
-      style: 'normal',
-    },
-    {
-      path: './gotham/Gotham-LightItalic.woff2',
-      weight: '300',
-      style: 'italic',
-    },
-    {
-      path: './gotham/Gotham-Medium.woff2',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: './gotham/Gotham-MediumItalic.woff2',
-      weight: '500',
-      style: 'italic',
-    },
-    {
-      path: './gotham/Gotham-Thin.woff2',
+      path: './inter/Inter18pt-Thin.woff2',
       weight: '100',
       style: 'normal',
     },
     {
-      path: './gotham/Gotham-ThinItalic.woff2',
+      path: './inter/Inter18pt-ThinItalic.woff2',
       weight: '100',
       style: 'italic',
     },
     {
-      path: './gotham/Gotham-XLight.woff2',
+      path: './inter/Inter18pt-ExtraLight.woff2',
       weight: '200',
       style: 'normal',
     },
     {
-      path: './gotham/Gotham-XLightItalic.woff2',
+      path: './inter/Inter18pt-ExtraLightItalic.woff2',
       weight: '200',
+      style: 'italic',
+    },
+    {
+      path: './inter/Inter18pt-Light.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: './inter/Inter18pt-LightItalic.woff2',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: './inter/Inter18pt-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './inter/Inter18pt-Italic.woff2',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: './inter/Inter18pt-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './inter/Inter18pt-MediumItalic.woff2',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: './inter/Inter18pt-SemiBold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './inter/Inter18pt-SemiBoldItalic.woff2',
+      weight: '600',
+      style: 'italic',
+    },
+    {
+      path: './inter/Inter18pt-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './inter/Inter18pt-BoldItalic.woff2',
+      weight: '700',
+      style: 'italic',
+    },
+    {
+      path: './inter/Inter18pt-ExtraBold.woff2',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: './inter/Inter18pt-ExtraBoldItalic.woff2',
+      weight: '800',
+      style: 'italic',
+    },
+    {
+      path: './inter/Inter18pt-Black.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: './inter/Inter18pt-BlackItalic.woff2',
+      weight: '900',
       style: 'italic',
     },
   ],
   display: 'swap',
   preload: true,
-  variable: '--font-family-gotham',
+  variable: '--font-family-inter',
 });
