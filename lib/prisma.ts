2edfd070e2d1071497073ff9e238b5be0ea20d20
Quentin Macheda <contact@qmweb.fr>
2025-05-22 22:50:58 +0200

    fix(docker): prisma and postgres connection

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,7 +4,11 @@ const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
 
-const prisma = globalForPrisma.prisma || new PrismaClient();
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    datasourceUrl: process.env.DATABASE_DOCKER_URL,
+  });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
