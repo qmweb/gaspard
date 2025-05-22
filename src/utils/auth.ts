2b121c85258105af18acfe826ca54d551b00e541
Quentin Macheda <contact@qmweb.fr>
2025-05-22 22:50:58 +0200

    fix(docker): prisma and postgres connection

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -4,7 +4,9 @@ import { prismaAdapter } from 'better-auth/adapters/prisma';
 // If your Prisma file is located elsewhere, you can change the path
 import { PrismaClient } from '@/app/generated/prisma';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasourceUrl: process.env.DATABASE_DOCKER_URL,
+});
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
