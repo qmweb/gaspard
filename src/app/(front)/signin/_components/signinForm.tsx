1c9d4d188a1df3db07d03b218bbefd0880ebf2fc
Quentin Macheda <contact@qmweb.fr>
2025-08-12 12:47:15 +0200

    refactor(auth): separate concerns in signin page

--- src/app/(front)/signin/_components/signinForm.tsx
+++ src/app/(front)/signin/_components/signinForm.tsx
@@ -11,13 +11,8 @@ import { useTranslation } from '@/hooks/useTranslation';
 
 import { Button } from '@/app/_components/ui/button';
 import { signIn } from '@/utils/lib/better-auth/auth-client';
-import { useTheme } from '@/utils/providers/ThemeProvider';
-
-import Logo from '~/images/logo_dark.svg';
-import LogoLight from '~/images/logo_light.svg';
 
 export default function SignIn() {
-  const { theme } = useTheme();
   const { t } = useTranslation();
 
   const [email, setEmail] = useState('');
@@ -38,107 +33,89 @@ export default function SignIn() {
   };
 
   return (
-    <main className='signin w-full h-screen flex justify-center items-center'>
-      <section className='signin__content'>
-        <div className='flex justify-center items-center'>
-          {theme === 'dark' ? (
-            <LogoLight className='signin__logo' />
-          ) : (
-            <Logo className='signin__logo' />
-          )}
-        </div>
-        <div className='grid gap-3 mt-6'>
-          <div className='grid gap-1'>
-            <label className='text-base font-medium' htmlFor='email'>
-              {t('auth.email')}
-            </label>
-            <Input
-              id='email'
-              type='email'
-              placeholder={t('auth.emailPlaceholder')}
-              required
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-              value={email}
-              size='large'
-              prefix={<Mail className='mt-0.5 me-1.5' color='gray' size={16} />}
-            />
-          </div>
+    <>
+      <div className='grid gap-1'>
+        <label className='text-base font-medium' htmlFor='email'>
+          {t('auth.email')}
+        </label>
+        <Input
+          id='email'
+          type='email'
+          placeholder={t('auth.emailPlaceholder')}
+          required
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          value={email}
+          size='large'
+          prefix={<Mail className='mt-0.5 me-1.5' color='gray' size={16} />}
+        />
+      </div>
 
-          <div className='grid gap-1'>
-            <label className='text-base font-medium' htmlFor='password'>
-              {t('auth.password')}
-            </label>
+      <div className='grid gap-1'>
+        <label className='text-base font-medium' htmlFor='password'>
+          {t('auth.password')}
+        </label>
 
-            <Input.Password
-              visibilityToggle={{
-                visible: passwordVisible,
-                onVisibleChange: setPasswordVisible,
-              }}
-              id='password'
-              type='password'
-              placeholder={t('auth.passwordPlaceholder')}
-              autoComplete='password'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              size='large'
-              prefix={<Lock className='mt-0.5 me-1.5' color='gray' size={16} />}
-            />
-          </div>
+        <Input.Password
+          visibilityToggle={{
+            visible: passwordVisible,
+            onVisibleChange: setPasswordVisible,
+          }}
+          id='password'
+          type='password'
+          placeholder={t('auth.passwordPlaceholder')}
+          autoComplete='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          size='large'
+          prefix={<Lock className='mt-0.5 me-1.5' color='gray' size={16} />}
+        />
+      </div>
 
-          <Button
-            type='submit'
-            className='mt-1'
-            disabled={loading}
-            onClick={async () => {
-              await signIn.email(
-                {
-                  email,
-                  password,
-                },
-                {
-                  onRequest: () => {
-                    setLoading(true);
-                  },
-                  onResponse: () => {
-                    setLoading(false);
-                  },
-                  onSuccess: () => {
-                    router.replace('/');
-                  },
-                  onError: (error) => {
-                    setLoading(false);
-                    const errorMessage =
-                      errorMessagesMap[error.error.message] ||
-                      t('auth.genericError');
-                    toast.error(errorMessage);
-                  },
-                },
-              );
-            }}
-          >
-            {loading ? (
-              <>
-                <LoaderCircle className='animate-spin' /> {t('auth.loading')}
-              </>
-            ) : (
-              <>{t('auth.loginButton')}</>
-            )}
-          </Button>
+      <Button
+        type='submit'
+        className='mt-1'
+        disabled={loading}
+        onClick={async () => {
+          await signIn.email(
+            {
+              email,
+              password,
+            },
+            {
+              onRequest: () => {
+                setLoading(true);
+              },
+              onSuccess: () => {
+                router.replace('/');
+              },
+              onError: (error) => {
+                setLoading(false);
+                const errorMessage =
+                  errorMessagesMap[error.error.message] ||
+                  t('auth.genericError');
+                toast.error(errorMessage);
+              },
+            },
+          );
+        }}
+      >
+        {loading ? (
+          <>
+            <LoaderCircle className='animate-spin' /> {t('auth.loading')}
+          </>
+        ) : (
+          <>{t('auth.loginButton')}</>
+        )}
+      </Button>
 
-          <p>
-            {t('auth.noAccountYet')}{' '}
-            <Link
-              replace={true}
-              className='text-base font-medium'
-              href='/signup'
-            >
-              {t('auth.contactAdmin')}
-            </Link>
-          </p>
-        </div>
-      </section>
-    </main>
+      <p>
+        {t('auth.noAccountYet')}{' '}
+        <Link replace={true} className='text-base font-medium' href='/signup'>
+          {t('auth.contactAdmin')}
+        </Link>
+      </p>
+    </>
   );
 }
